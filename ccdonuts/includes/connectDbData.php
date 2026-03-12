22e2a75bca0ffc9a485ea2927f5020eaa61126ed
nf0926poutt-jpg <nf.0926.poutt@gmail.com>
2026-03-12 13:37:52 +0900

    Delete ccdonuts directory

--- ccdonuts/includes/connectDbData.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('ACCESSDB', 'mysql:host=localhost;dbname=nf0926poutt_ccdonuts;charset=utf8');
-define('DBID', 'nf0926poutt_ccdonuts');
-define('DBPW', 'ccdonuts2025cca');
-?>
--- ccdonuts/includes/localDBdata.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('ACCESSDB', 'mysql:host=localhost;dbname=ccdonuts;charset=utf8');
-define('DBID', 'ccStaff');
-define('DBPW', 'ccDonuts');
-?>
